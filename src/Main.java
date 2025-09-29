092f7837b0e831d413752268ff159c4a039ef4a9
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-09-29 11:40:29 +0100

    Tester la connexion  et gérer les exceptions SQL. : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/TRAN-6

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.sql.Connection;
+
+import dataeBase.ConectionDB;
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            ConectionDB db = ConectionDB.getInstance();
+            Connection conn = db.getConnection();
+            System.out.println("Test de connexion réussi !");
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
