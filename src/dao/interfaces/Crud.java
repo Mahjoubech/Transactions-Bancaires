333e0227ea89820152a97fd31a2121b78f81b7dd
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-09-29 16:08:47 +0100

    Créer l’interface Crud<T> contenant les méthodes (save, update, delete, findById, findAll) : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/TRAN-23

---
+++ src/dao/interfaces/Crud.java
@@ -0,0 +1,12 @@
+package dao.interfaces;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Crud <T> {
+    void create(T Obj);
+    void update(T Obj);
+    void delete(String id);
+    Optional<T> findById(String id);
+    List<T> findAll();
+}
