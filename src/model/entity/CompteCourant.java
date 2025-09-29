41d5cadb7e396f586d225cf5c76856af01ed08db
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-09-29 14:47:12 +0100

    Créer final CompteCourant (hérite Compte, decouvertAutorise) : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/TRAN-12

--- src/model/entity/CompteCourant.java
+++ src/model/entity/CompteCourant.java
@@ -1,26 +1,16 @@
-package entity;
-import util.Validateur;
-public class CompteCourant extends Compte {
+package model.entity;
+import model.enums.typeCompte;
+public final class CompteCourant extends Compte {
     private double decouvert;
-    public CompteCourant(String code, double solde, double decouvert) {
-        super(code, solde);
-        if (!Validateur.isPositiveDouble(decouvert)) throw new IllegalArgumentException("Découvert doit être positif !");
-        this.decouvert = decouvert;
-    }
-    public double getDecouvert() { return decouvert; }
-    @Override
-    public void retirer(double montant, String destination) throws Exception {
-        if (!Validateur.isPositiveAmount(montant)) throw new Exception("Montant négatif ou nul !");
-        if (solde - montant < -decouvert) throw new Exception("Solde insuffisant (dépassement du découvert autorisé) !");
-        ajouterOperation(new Retrait(montant, destination));
-        solde -= montant;
-    }
-    @Override
-    public double calculerInteret() { return 0; }
-    @Override
-    public void afficherDetails() {
-        System.out.println(util.ConsoleColor.colorize(util.ConsoleColor.BLUE, "CompteCourant [" + code + "] Solde: " + solde + " Découvert: " + decouvert));
-        System.out.println(util.ConsoleColor.colorize(util.ConsoleColor.CYAN, "Historique opérations:"));
-        listeOperations.forEach(op -> System.out.println(util.ConsoleColor.colorize(util.ConsoleColor.YELLOW, op.toString())));
+   public  CompteCourant(String id, String num, String idClient, double solde, typeCompte typeCompte ,double decouvert ) {
+       super(id, num, idClient, solde, typeCompte);
+       this.decouvert = decouvert;
+
+   }
+   public double getDecouvert() {
+         return decouvert;
+   }
+    public void setDecouvert(double decouvert) {
+            this.decouvert = decouvert;
     }
 }
