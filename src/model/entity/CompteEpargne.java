ec0241e07d397a38828d845111c0a6b367fd6532
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-09-29 14:47:47 +0100

    Créer final CompteEpargne (hérite Compte, taux) : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/TRAN-12

--- src/model/entity/CompteEpargne.java
+++ src/model/entity/CompteEpargne.java
@@ -1,27 +1,14 @@
-package entity;
-import util.Validateur;
+package model.entity;
+import model.enums.typeCompte;
 
-public class CompteEpargne extends Compte {
+public final class CompteEpargne extends Compte {
     private double tauxInteret;
-    public CompteEpargne(String code, double solde, double tauxInteret) {
-        super(code, solde);
-        if (!Validateur.isPositiveDouble(tauxInteret)) throw new IllegalArgumentException("Taux doit être positif !");
+    public CompteEpargne(String id, String num, String idClient, double solde, typeCompte typeCompte , double tauxInteret) {
+        super(id, num, idClient, solde, typeCompte);
         this.tauxInteret = tauxInteret;
     }
     public double getTauxInteret() { return tauxInteret; }
-    @Override
-    public void retirer(double montant, String destination) throws Exception {
-        if (!Validateur.isPositiveAmount(montant)) throw new Exception("Montant négatif ou nul !");
-        if (solde < montant) throw new Exception("Solde insuffisant !");
-        ajouterOperation(new Retrait(montant, destination));
-        solde -= montant;
-    }
-    @Override
-    public double calculerInteret() { return solde * tauxInteret / 100.0; }
-    @Override
-    public void afficherDetails() {
-        System.out.println(util.ConsoleColor.colorize(util.ConsoleColor.MAGENTA, "CompteEpargne [" + code + "] Solde: " + solde + " Taux: " + tauxInteret + "%"));
-        System.out.println(util.ConsoleColor.colorize(util.ConsoleColor.CYAN, "Historique opérations:"));
-        listeOperations.forEach(op -> System.out.println(util.ConsoleColor.colorize(util.ConsoleColor.YELLOW, op.toString())));
+    public void setTauxInteret(double tauxInteret) {
+        this.tauxInteret = tauxInteret;
     }
 }
