5dacd2a8e4f6b2941be1c3104e5e07fa83c052ed
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-09-30 01:34:58 +0100

    Écrire tests unitaires pour ClientService (JUnit / Mockito) : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/TRAN-42

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
 import java.sql.Connection;
 
+import dao.interfaces.ClientRepository;
+import dao.repository.ClientRepoImp;
 import dataeBase.ConectionDB;
+import service.ClientService;
+import service.interfaces.ClientServiceInterface;
+import ui.ClientMenu;
+
 public class Main {
     public static void main(String[] args) {
         try {
-            ConectionDB db = ConectionDB.getInstance();
-            Connection conn = db.getConnection();
+            Connection conn  = ConectionDB.getInstance().getConnection();
+            ClientRepository clientRepo = new ClientRepoImp(conn);
+            ClientServiceInterface clientService = new ClientService(clientRepo);
+            ClientMenu hh = new ClientMenu(clientService);
+            hh.affiche();
             System.out.println("Test de connexion réussi !");
             conn.close();
         } catch (Exception e) {
--- src/test/ClientTest.java
+++ src/test/ClientTest.java
@@ -1,4 +1,37 @@
 package test;
 
+import dao.repository.ClientRepoImp;
+import model.entity.Client;
+
+import java.util.UUID;
+
 public class ClientTest {
+//    public static void main(String[] args) {
+//        String uuidPart = UUID.randomUUID().toString().replace("-", "").substring(0, 20);
+//        ClientRepoImp repo = new ClientRepoImp();
+//        Client c = new Client(
+//                "CPT-" + uuidPart.toUpperCase(),
+//                "Mohamed El Amrani",
+//                "hh.amrani@test.com"
+//        );
+//        Client c1 = new Client("CPT-" + uuidPart.toUpperCase(), "Mohamed El Amrani", "hhf.amrani@test.com");
+//        Client c2 = new Client("CPT-" + UUID.randomUUID().toString().replace("-", "").substring(0, 20).toUpperCase(), "Mahjoub Echerkaoui", "cher@gmail.com");
+////        Client c3 = new Client("CPT-" + UUID.randomUUID().toString().replace("-", "").substring(0, 20).toUpperCase(), "Mahjoub Echerkaoui", "hhfg.amrani@test.com");
+//        Client c4 = new Client("CPT-" + UUID.randomUUID().toString().replace("-", "").substring(0, 20).toUpperCase(), "Mohamed El Amrani", "client4@gmail.com");
+//        Client c5 = new Client("CPT-" + UUID.randomUUID().toString().replace("-", "").substring(0, 20).toUpperCase(), "Mohamed El Amrani", "client5@gmail.com");
+//        Client c6 = new Client( "CPT-" + UUID.randomUUID().toString().replace("-", "").substring(0, 20).toUpperCase(), "Mahjoub Echerkaoui" , "cherk@gmail.com");
+//        repo.findAll().forEach(System.out::println);
+//        System.out.println("delete : ");
+////      repo.create(c4);
+////      repo.create(c5);
+////jjjj
+//        System.out.println(" recherche par nom : ");
+//        repo.findByName("Mohamed El Amrani").forEach(System.out::println);
+//        System.out.println(" recherche par email : ");
+//        System.out.println(repo.findByEmail(
+//                "cherk@gmail.com"));
+//        System.out.println("After : ");
+//        repo.findAll().forEach(System.out::println);
+//        System.out.println("✅ Client inséré avec succès !");
+//    }
 }
