fba86db08dbf73200de1e2162cebd916c7f632c2
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-09-30 01:20:24 +0100

    Creation class Helper pour generate code (id , num ) : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/TRAN-48

--- src/util/Helper.java
+++ src/util/Helper.java
@@ -1,4 +1,10 @@
 package util;
 
+import java.util.UUID;
+
 public class Helper {
+    public static String generateIDNum(){
+        String uuidPart = UUID.randomUUID().toString().replace("-", "").substring(0, 20);
+        return "CPT - " + uuidPart.toUpperCase();
+    }
 }
