e761c5467e03f766f7caaf6ed8fbaa95be94a04e
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-09-30 01:18:50 +0100

    Implémenter ClientService qui implémente ClientServiceInterface : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/TRAN-38

--- src/service/interfaces/ClientServiceInterface.java
+++ src/service/interfaces/ClientServiceInterface.java
@@ -1,4 +1,17 @@
 package service.interfaces;
 
+import model.entity.Client;
+import java.util.List;
+import java.util.Optional;
+
 public interface ClientServiceInterface {
+    void create(String nom , String email);
+    void update(String id , String nom , String email);
+    void delete(Client c);
+    Optional<Client> findById(String id);
+    List<Client> getAllClients();
+    List<Client> getByNom(String name);
+    Optional<Client> getByEmail(String email);
+
 }
+
