b84ec8298e66ec2a3e925e1091a395b101288dd7
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-10-01 15:56:55 +0100

    Renommer la méthode generateIDNum en generateIDClient et ajouter des méthodes pour générer des codes et numéros de compte : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/TRAN-48

--- src/util/Helper.java
+++ src/util/Helper.java
@@ -1,10 +1,20 @@
 package util;
 
+import java.util.Random;
 import java.util.UUID;
-
 public class Helper {
-    public static String generateIDNum(){
+    public static String generateIDClient() {
         String uuidPart = UUID.randomUUID().toString().replace("-", "").substring(0, 20);
-        return "CPT - " + uuidPart.toUpperCase();
+        return "CLI - " + uuidPart.toUpperCase();
+    }
+    public static String generateCompteCode() {
+        Random rand = new Random();
+        int number = rand.nextInt(100000); // 0 à 99999
+        return String.format("CPT - %05d", number);
+    }
+    public static String generateCompteNum() {
+        String datePart = java.time.LocalDate.now().toString().replace("-", "");
+        String randomPart = UUID.randomUUID().toString().substring(0, 6).toUpperCase();
+        return "ACC - " + datePart + "-" + randomPart;
     }
 }
