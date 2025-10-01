363dcbbaf88f133098b3dd2b183ab3bdfbc210ac
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-10-01 16:15:55 +0100

    Ajouter la gestion des comptes dans la classe Main avec l'intégration de CompteService et l'affichage du menu : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/TRAN-42

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 import java.sql.Connection;
 
 import dao.interfaces.ClientRepository;
+import dao.interfaces.CompteRepository;
 import dao.repository.ClientRepoImp;
+import dao.repository.CompteRepoImp;
 import dataeBase.ConectionDB;
 import service.ClientService;
+import service.CompteService;
 import service.interfaces.ClientServiceInterface;
+import service.interfaces.CompteServiceInterface;
 import ui.ClientMenu;
+import ui.CreationCompteMenu;
+import ui.Menu;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,8 +19,12 @@ public class Main {
             Connection conn  = ConectionDB.getInstance().getConnection();
             ClientRepository clientRepo = new ClientRepoImp(conn);
             ClientServiceInterface clientService = new ClientService(clientRepo);
-            ClientMenu hh = new ClientMenu(clientService);
-            hh.affiche();
+            CompteRepository comptetRepo = new CompteRepoImp(conn);
+            CompteServiceInterface compteService = new CompteService(comptetRepo);
+            ClientMenu hh = new ClientMenu(clientService , compteService);
+            CreationCompteMenu kk = new CreationCompteMenu( compteService);
+            Menu JJ = new Menu( hh, kk);
+            JJ.afficheMenu();
             System.out.println("Test de connexion réussi !");
             conn.close();
         } catch (Exception e) {
