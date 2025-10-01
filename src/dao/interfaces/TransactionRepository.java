2d44095ca815f0b0cc2a2ca512c49e09479087e6
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-10-01 17:14:47 +0100

    Ajouter la méthode findByCompte dans l'interface TransactionRepository pour récupérer les transactions par compte : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/TRAN-23

--- src/dao/interfaces/TransactionRepository.java
+++ src/dao/interfaces/TransactionRepository.java
@@ -1,4 +1,11 @@
 package dao.interfaces;
 
-public interface TransactionRepository {
+import model.entity.Transaction;
+import model.enums.typeTransaction;
+
+import java.util.Date;
+import java.util.List;
+
+public interface TransactionRepository extends Crud<Transaction> {
+    List<Transaction> findByCompte(String idCompte);
 }
