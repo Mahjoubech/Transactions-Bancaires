6e50bb7b88a975bda1929afbf0e6c47c681aa61d
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-10-03 01:50:04 +0100

    Ajouter l'interface TransactionServiceInterface pour gérer les opérations sur les transactions : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/TRAN-38

---
+++ src/service/TransactionServiceInterface.java
@@ -0,0 +1,26 @@
+package service;
+
+import entity.Transaction;
+import enums.typeTransaction;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public interface TransactionServiceInterface {
+    void ajoute(double amount, String idCompte, typeTransaction type, String lieu);
+    Optional<Transaction> findById(String id);
+    List<Transaction> findByMontant(Double montant);
+    List<Transaction> findByDate(Date dt);
+    List<Transaction> getAll();
+    List<Transaction> findByType(typeTransaction type);
+    List<Transaction> findByLieu(String lieu);
+    Map<typeTransaction, List<Transaction>> groupByType();
+    Map<String, List<Transaction>> groupByDate();
+    double totalByCompte(String idCompte);
+    double moyenneByCompte(String idCompte);
+    double totalByClient(String idClient);
+    double moyenneByClient(String idClient);
+    List<Transaction> detecterTransactionsSuspectes(double seuilMontant, String idClient);
+}
